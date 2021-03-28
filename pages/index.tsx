c2319d8848b3d5934f46c26a08d13c10ba5cb8a6
Filipe Soares <filipeseventeen1@gmail.com>
2021-03-28 16:18:26 -0300

    add global style and home style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import Head from 'next/head'
+import { GlobalStyle } from '../GlobalStyle'
 import Body from '../src/components/Body'
+import { Container } from '../src/components/Home/style'
 
 export default function Home() {
   return (
-    <div>
+    <Container>
+      <GlobalStyle />
       <Head>
         <title>Login</title>
       </Head>
       <Body />
-    </div>
+    </Container>
   )
 }
