2fedfe46db6e444e6474a0f6512840bb08370132
Filipe Soares <filipeseventeen1@gmail.com>
2021-03-28 16:02:17 -0300

    add default component

---
+++ src/components/Component/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container }  from './style'
+
+const ComponentName: React.FC = () => {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default ComponentName
